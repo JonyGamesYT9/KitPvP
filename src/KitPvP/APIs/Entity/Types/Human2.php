2b48057703134edacdbb6493a4566e980c6f6963
Jony <68771903+JonyGamesYT9@users.noreply.github.com>
2021-08-06 11:52:59 -0500

    Create Human2.php

---
+++ src/KitPvP/APIs/Entity/Types/Human2.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace KitPvP\APIs\Entity\Types;
+
+use pocketmine\entity\Human;
+
+/**
+ * Class Human2
+ * Extends Human
+ */
+class Human2 extends Human {
+  
+  /**
+   * @return string
+   */
+  function getName() : string {
+  return '';
+  }
+}
+?>
