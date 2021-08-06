8377f137681b1011a47c0a219b5702e9acccda64
Jony <68771903+JonyGamesYT9@users.noreply.github.com>
2021-08-06 11:53:34 -0500

    Create KillsTops.php

---
+++ src/KitPvP/APIs/Entity/Types/KillsTops.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace KitPvP\APIs\Entity\Types;
+
+use pocketmine\entity\Human;
+
+/**
+ * Class Human2
+ * Extends Human
+ */
+class KillsTops extends Human {
+  
+  /**
+   * @return string
+   */
+  function getName() : string {
+  return '';
+  }
+}
+?>
